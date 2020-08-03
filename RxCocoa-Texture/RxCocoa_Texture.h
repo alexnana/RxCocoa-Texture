840b4cc73ced7f5690b316fd2fcac49ec7421b12
Alex Hung <abcalex@gmail.com>
2020-08-03 23:14:05 +0800

    🔧 add carthage support

---
+++ RxCocoa-Texture/RxCocoa_Texture.h
@@ -0,0 +1,19 @@
+//
+//  RxCocoa_Texture.h
+//  RxCocoa-Texture
+//
+//  Created by Alex Hung on 3/8/2020.
+//  Copyright © 2020 Alex Hung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxCocoa_Texture.
+FOUNDATION_EXPORT double RxCocoa_TextureVersionNumber;
+
+//! Project version string for RxCocoa_Texture.
+FOUNDATION_EXPORT const unsigned char RxCocoa_TextureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxCocoa_Texture/PublicHeader.h>
+
+
